92bb8639f863c9cda9a6737bf3516a0b79a3d9a9
slabwu <134193743+slabwu@users.noreply.github.com>
2024-04-13 19:25:40 +0800

    Edit copy of forms & error states

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ inputs.forEach((input) => input.addEventListener('blur', (e) => {
     if (e.target.checkValidity()) {
         e.target.nextElementSibling.textContent = "";
     } else if (e.target.validity.typeMismatch && e.target.getAttribute('id') == "email") {
-        e.target.nextElementSibling.textContent = "Please input an email address";
+        e.target.nextElementSibling.textContent = "Please input a valid email address";
     } else if (e.target.matches(':invalid') && e.target.getAttribute('id') == "phone") {
-        e.target.nextElementSibling.textContent = "Please input a phone number";
+        e.target.nextElementSibling.textContent = "Please input a 10 digit phone number";
     } else if (e.target.value != document.querySelector('#password').value && e.target.getAttribute('id') == "confirm_password") {
         e.target.nextElementSibling.textContent = "Passwords do not match";
     } else if (e.target.validity.tooShort) {
@@ -22,7 +22,7 @@ button.addEventListener('click', (e) => {
         inputs.forEach((input) => {
             if (input.value == "") empty = true;
         })
-        empty? alert('Please fill in the entire form!') : alert('Form successfully submitted!');
+        empty? alert('Please complete the entire form! 😾') : alert('Purrfect! You have successfully registered for an account 🐾');
    } else {
         e.preventDefault();
    }
